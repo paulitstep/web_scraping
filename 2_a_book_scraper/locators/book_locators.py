dceeba0de8cd81014b1831b73e6ca5fa4b0e880d
paul <pasha-mo1@rambler.ru>
2020-07-28 11:27:03 +0300

    Adding A Book Scraper

---
+++ 2_a_book_scraper/locators/book_locators.py
@@ -0,0 +1,5 @@
+class BookLocators:
+    NAME_LOCATOR = 'article.product_pod h3 a'
+    LINK_LOCATOR = 'article.product_pod h3 a'
+    PRICE_LOCATOR = 'article.product_pod p.price_color'
+    STAR_RATING = 'article.product_pod p.star-rating'
