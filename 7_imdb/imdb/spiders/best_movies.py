047373d70c2e91403bdd6d7be56c0bac5c72be66
paul <pasha-mo1@rambler.ru>
2020-08-06 12:15:29 +0300

    Adding IMDb Project

---
+++ 7_imdb/imdb/spiders/best_movies.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['imdb.com']
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//td[@class='titleColumn']/a"), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        yield {
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
+            'genre': response.xpath("////div[@class='subtext']/a[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url': response.url
+        }
