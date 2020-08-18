e3973022fa4aec82e817111fa92305c55a4f69f1
paul <pasha-mo1@rambler.ru>
2020-08-18 12:21:46 +0300

    Adding SlikDeals Project

---
+++ 9_slikdeals/silkdeals/spiders/example.py
@@ -0,0 +1,37 @@
+import scrapy
+from scrapy.selector import Selector
+from scrapy_selenium import SeleniumRequest
+
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://duckduckgo.com/',
+            wait_time=3,
+            screenshot=True,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        # img = response.meta['screenshot']
+
+        # with open('screenshot.png', 'wb') as f:
+        #     f.write(response.request.meta['screenshot'])
+
+        driver = response.meta['driver']
+        search_input = driver.find_element_by_xpath("//input[@id='search_form_input_homepage']")
+        search_input.send_keys('Hello World')
+
+        search_btn = driver.find_element_by_id("search_button_homepage")
+        search_btn.click()
+
+        html = driver.page_source
+        response_obj = Selector(text=html)
+
+        links = response_obj.xpath("//div[@class='result__extras__url']/a")
+        for link in links:
+            yield {
+                'url': link.xpath(".//@href").get()
+            }
